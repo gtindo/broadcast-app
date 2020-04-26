d6a848561155d6abb6cf6438cbf54f6310e2e1c3
gtindo <gtyletindo@gmail.com>
2020-04-26 20:42:04 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# BROADCAST APP
---
+++ client/js/main.js
@@ -0,0 +1,115 @@
+
+// Set constraints on stream
+
+const mediaConstraints = {
+  video: {
+    optional: [
+      { maxWidth: 720 },
+      { maxHeight: 480 }
+    ]
+  },
+};
+
+// Video element where stream will be placed
+const localVideo = document.querySelector("#local-video");
+
+// Local stream where stream will be placed
+let localStream;
+
+// Stream call backs
+
+// Handle success by adding the mediastream to video element
+function gotLocalMediaStream(mediaStream)
+{ 
+  localStream = mediaStream;
+  localVideo.srcObject = mediaStream; 
+}
+
+// Handle error by logging a message to console with error message
+function handleLocalMediaStreamError(error) {
+  console.log('navigator.getUserMedia error: ', error)
+}
+
+// Initializes media stream
+navigator.mediaDevices.getUserMedia(mediaConstraints)
+  .then(gotLocalMediaStream)
+  .catch(handleLocalMediaStreamError)
+
+/*****************************************************/
+const configuration = null;
+let rtcConnection = new RTCPeerConnection(configuration);
+
+// Set up to exchange only video
+const offerOptions = {
+  offerToReceiveVideo: 1,
+};
+
+// Log offer creation and sets peer connection session description
+function createdOffer(description){
+  console.log("offer")
+  console.log(description)
+  rtcConnection.setLocalDescription(description)
+    .then(() => {console.log("Local description has been set.")})
+    .catch((err) => {console.log(err)})
+}
+
+function setSessionDescriptionError(){
+  console.log("Error while setting session description")
+}
+
+
+
+// Create peer connection and behavior
+
+function connect(){
+
+
+
+console.log(localStream)
+// Add localstream to connection and create offer to connect
+rtcConnection.addTrack(localStream.getTracks()[0])
+
+rtcConnection.createOffer(offerOptions)
+  .then(createdOffer)
+  .catch(setSessionDescriptionError);
+
+}
+
+/*****************************************************/
+
+function stopBroadcast(){
+  rtcConnection.close();
+  rtcConnection = null;
+}
+
+/* Protocols
+SDP : Session description protocol
+ICE : Interactive connectivity establishment
+STUN :
+TURN :
+RTP :
+*/
+
+/* Emitter : Web Client
+1. Create RTCPeerConnection
+2. call RTCPeerConnection.createOffer()
+3. call RTCPeerConnection.setLocalDescription()
+4. Generate ice candidates with STUN Server or TURN Server (set in servers configuration)
+5. Send offer to intended receiver ()
+
+12. client receive Answer
+13. client call RTCPeerConnection.setRemoteDescription()
+
+END;
+*/
+
+/* Receiver : Web Server
+
+6. Create RTCPeerConnection
+7. call RTCPeerConnection.setRemoteDescription()
+8. call RTCPeerConnection.addTrack()
+9. call RTCPeerConnection.createAnswer()
+10. call RTCPeerConnection.setLocalDescription()
+11. Send answer to the caller
+
+*/
